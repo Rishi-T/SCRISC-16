6540f049e463e2b39cbde6e31e9346e5cf2b34b2
Rishi-T <rishithotli@outlook.com>
2023-04-17 20:48:14 +0530

    Barrel Shifter bugfix
    Fixed a bug in in the barrel shifter where the 11 most significant bits of the shift amount would've been ignored

--- Verilog Files/bar_shift_16b.v
+++ Verilog Files/bar_shift_16b.v
@@ -3,16 +3,20 @@
 module bar_shift_16b(
     output [15:0] out,
     input [15:0] in,
-    input [4:0] sh,
+    input [15:0] sh,
     input sign,
     input cont
     );
     
     wire [15:0] li,lo,ri,ro;
-    wire [4:0] ls,rs;
+    wire [4:0] ls,rs,shin;
+    wire shall;
+    
+    assign shall = |(sh[15:5]);
+    assign shin = {5{shall}} | sh[4:0];
     
     demux_1x2_16b d16(li,ri,in,cont);
-    demux_1x2_5b d5(ls,rs,sh,cont);
+    demux_1x2_5b d5(ls,rs,shin,cont);
     
     left_shifter lsh(lo,li,ls);
     right_shifter rsh(ro,ri,rs,sign);
